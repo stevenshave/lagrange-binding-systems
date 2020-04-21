89dced0a09c8aadd5cd11c121ebe48eabc27919c
Steven Shave <stevenshave@gmail.com>
2020-04-21 22:40:34 +0100

    Added homodimer formation

---
+++ homodimer_formation_lagrange.py
@@ -0,0 +1,21 @@
+"""
+homodimer formation binding system solved using Lagrange multiplier approach
+
+Lagrange multiplier example of homodimer formation system
+"""
+
+from scipy.optimize import fsolve
+import autograd.numpy as np
+from autograd import grad
+def lagrange_homodimerformation(p0,kdpp):
+    def F(X): # Augmented Lagrange function
+        response=((X[0]*X[0])/kdpp)
+        constraint1=p0-(response*2+X[0])
+        return response - X[1] *constraint1
+    dfdL = grad(F, 0) # Gradients of the Lagrange function
+    pf, lam1 = fsolve(dfdL, [p0, 1.0])
+    return {'pf':pf,'pp':((pf*pf)/kdpp)}
+
+if __name__ == "__main__":
+    concentrations=lagrange_homodimerformation(20,5)
+    print(f"The answer is at {concentrations['pp']}")
